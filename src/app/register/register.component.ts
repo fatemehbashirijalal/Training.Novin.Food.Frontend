b030a006b71a6adc3a31e6810aa6bbc2f6cc5890
Fatemeh <fatemehbashirijalal@gmail.com>
2023-06-21 08:37:56 +0200

    try git

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -19,5 +19,30 @@ export class RegisterComponent {
   restownerfullname=new FormControl('', [Validators.required, Validators.minLength(8)]);
   restownerphon=new FormControl('', [Validators.required, Validators.minLength(8)]);
   restaurantname=new FormControl('',[Validators.required,Validators.minLength(2)]);
-  restauranaddress=new FormControl('',[Validators.required])
+  restauranaddress=new FormControl('',[Validators.required]);
+  restownerusername =new FormControl('', [Validators.required, Validators.minLength(8)]);
+  restownerpassword= new FormControl('', [Validators.required, Validators.minLength(8)]);
+register(){
+  if(this.selectedOption == 'customer'){
+    this.RegisterServices.Register(
+      this.fullname.value ?? '',
+      this.phone.value ?? '',
+      this.email.value ?? '',
+      this.address.value ?? '',
+      this.username.value??'',
+      this.password.value??'',)
+    }
+
+    else if (this.selectedOption == 'restaurant'){
+      this.RegisterServices.Register(
+        this.restownerfullname.value ?? '',
+        this.restownerphon.value ?? '',
+        this.restaurantname.value ?? '',
+        this.restauranaddress.value ?? '',
+        this.restownerusername.value ?? '',
+        this.restownerpassword.value ?? '',
+      )
+    }
 }
+}
+
